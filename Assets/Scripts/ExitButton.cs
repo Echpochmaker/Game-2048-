4e376ac434ff7c1f8a0072d180d569453efdf31d
Echpochmaker <pasha.bobyliov@yandex.ru>
2022-11-30 14:37:29 +0300

    Settings and start screens

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    /// <summary>
+    /// Метод для выхода из игры
+    /// </summary>
+    public void ExitPaling() => Application.Quit();
+}
---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    /// <summary>
+    /// Метод для переключений между экранами (сценами) игры
+    /// </summary>
+    /// <param name="sceneNumber">Номер сцены по списку</param>
+    public void NextScene(int sceneNumber) => SceneManager.LoadScene(sceneNumber);    
+}
