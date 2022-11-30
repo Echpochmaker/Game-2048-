386275db8163e09eafb3680fecc809e0bcc84a5d
Echpochmaker <pasha.bobyliov@yandex.ru>
2022-11-30 19:31:24 +0300

    Added on/of audio button in settings

---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour
+{
+    [SerializeField] GameObject onAudioButton;
+    [SerializeField] GameObject offAudioButton;
+
+    /// <summary>
+    /// Кнопка ВКЛ
+    /// </summary>
+    public void OnAudio()
+    {
+        offAudioButton.SetActive(false);
+        onAudioButton.SetActive(true);
+    }
+
+    /// <summary>
+    /// Кнопка ВКЛ
+    /// </summary>
+    public void OffAudio()
+    {
+        onAudioButton.SetActive(false);
+        offAudioButton.SetActive(true);
+    }
+}
